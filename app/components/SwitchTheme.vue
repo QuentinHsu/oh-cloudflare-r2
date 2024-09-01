be6124032379c1176cf7041194f62afdcdebd80f
QuentinHsu <xuquentinyang@gmail.com>
2024-09-01 09:12:35 +0800

    refactor: update SwitchTheme component to use div instead of button

--- app/components/SwitchTheme.vue
+++ app/components/SwitchTheme.vue
@@ -96,7 +96,7 @@ watch(() => $colorMode.value, (newColorMode) => {
 </script>
 
 <template>
-  <button type="button" class="btn-reset" @click="toggleTheme">
+  <div class="cursor-pointer" @click="toggleTheme">
     <client-only>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100%" height="100%">
         <g>
@@ -122,13 +122,5 @@ watch(() => $colorMode.value, (newColorMode) => {
         <circle cx="50" cy="50" r="30" fill="currentColor" mask="url(#themeToggleMask)" />
       </svg>
     </client-only>
-  </button>
+  </div>
 </template>
-
-<style lang="scss" scoped>
-.btn-reset {
-  all: unset;
-  display: inline-block;
-  cursor: pointer;
-}
-</style>import { useNuxtApp, useHead } from 'nuxt/app';
