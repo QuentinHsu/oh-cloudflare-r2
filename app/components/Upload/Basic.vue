8e6bafae37b9c0a57521b079f315263680fdc213
QuentinHsu <xuquentinyang@gmail.com>
2024-09-01 20:17:55 +0800

    feat: add current path support to file upload functionality

--- app/components/Upload/Basic.vue
+++ app/components/Upload/Basic.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import type { RequestMethodResponse, UploadFile } from 'tdesign-vue-next'
+import { useStoreFileManager } from '~/stores/useStoreFileManager'
+import { useStoreLogin } from '~/stores/useStoreLogin'
 
+const storeFileManager = useStoreFileManager()
+
+const currentPath = ref<string>('')
 async function onUpload(files: UploadFile): Promise<RequestMethodResponse> {
-  // Add your upload logic here
   files = toRaw(files[0])
-  const prefix = 'test/new'
-  const upload = useUpload(`/api/blob?prefix=${prefix}`, { method: 'PUT', params: {
-
+  const prefix = currentPath.value
+  const storeLogin = useStoreLogin()
+  const upload = useUpload(`/api/blob?prefix=${prefix}`, { method: 'PUT', headers: {
+    Authorization: `Bearer ${storeLogin.token || ''}`,
   } })
   if (Array.isArray(files)) {
     // Handle the case when files is an array
@@ -20,16 +25,28 @@ async function onUpload(files: UploadFile): Promise<RequestMethodResponse> {
   }
   return { status: 'fail', error: '上传失败', response: { url: undefined } }
 }
+function init() {
+  currentPath.value = storeFileManager.currentPath
+}
+onMounted(() => {
+  init()
+})
 </script>
 
 <template>
   <div class="upload__basic">
-    <div class="upload__basic__header" />
     <div class="upload__basic__content">
-      <t-upload
-        theme="file-flow"
-        :request-method="onUpload" :auto-upload="false" :upload-button="undefined" :cancel-upload-button="{ theme: 'default', content: '取消上传' }" multiple :max="8" tips="支持粘贴上传"
-      />
+      <t-form label-align="top">
+        <t-form-item label="Upload path" name="uploadPath">
+          <t-input v-model="currentPath" placeholder="Please enter the upload path" />
+        </t-form-item>
+        <t-form-item label="Upload files" name="uploadFiles">
+          <t-upload
+            theme="file-flow"
+            :request-method="onUpload" :auto-upload="false" :upload-button="undefined" :cancel-upload-button="{ theme: 'default', content: '取消上传' }" multiple :max="8" tips="支持粘贴上传"
+          />
+        </t-form-item>
+      </t-form>
     </div>
   </div>
 </template>
