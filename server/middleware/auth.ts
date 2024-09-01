125c2a858151dd6c94e61046fb42d40782483129
QuentinHsu <xuquentinyang@gmail.com>
2024-09-01 19:46:24 +0800

    refactor: improve login token handling in auth middleware, enforced type consistency

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,6 +1,7 @@
 export default eventHandler((event) => {
-  const loginToken = getHeader(event, 'Authorization')?.replace('Bearer ', '')
-  if (event.path.startsWith('/api/') && !event.path.startsWith('/api/_') && loginToken !== useRuntimeConfig(event).loginToken) {
+  const loginToken = String(getHeader(event, 'Authorization')?.replace('Bearer ', ''))
+  const correctToken = String(useRuntimeConfig(event).loginToken)
+  if (event.path.startsWith('/api/') && !event.path.startsWith('/api/_') && loginToken !== correctToken) {
     throw createError({
       status: 401,
       message: 'Unauthorized',
