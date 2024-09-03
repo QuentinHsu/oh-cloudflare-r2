44b01c9aa21e20f25bacad69cb2c5c53d9bd5dcd
QuentinHsu <xuquentinyang@gmail.com>
2024-09-03 17:11:22 +0800

    refactor: update Header component to use cursor-pointer for logo click event

--- app/components/Header/index.vue
+++ app/components/Header/index.vue
@@ -7,7 +7,7 @@ function onClickLogo() {
 
 <template>
   <t-header class="flex justify-between items-center p-x-10px">
-    <div class="flex items-center space-x-10px" @click="onClickLogo">
+    <div class="flex items-center space-x-10px cursor-pointer" @click="onClickLogo">
       <img src="/icon-128x128.png" alt="Cloudflare R2" class="w-30px h-30px">
       <h1 class="text-base">
         Oh Cloudflare R2
