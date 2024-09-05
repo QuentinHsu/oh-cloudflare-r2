bcdb023ab938584c62ef2f4c6b7dc7c204fa3ca9
QuentinHsu <xuquentinyang@gmail.com>
2024-09-05 17:39:39 +0800

    refactor: update SwitchTheme component to include flex layout for logo click event

--- app/components/SwitchTheme.vue
+++ app/components/SwitchTheme.vue
@@ -96,7 +96,7 @@ watch(() => $colorMode.value, (newColorMode) => {
 </script>
 
 <template>
-  <div class="cursor-pointer" @click="toggleTheme">
+  <div class="cursor-pointer flex items-center" @click="toggleTheme">
     <client-only>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100%" height="100%">
         <g>
